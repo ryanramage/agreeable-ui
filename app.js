322adcbda3b59cd8a74d91ebcccfaa8d7bb4d5e5
Ryan Ramage <ryan.ramage@gmail.com>
2024-03-24 18:16:34 -0600

    working

--- app.js
+++ app.js
@@ -2,29 +2,60 @@ import DHT from 'hyperdht'
 import b4a from 'b4a'
 import Protomux from 'protomux'
 import Channel from 'jsonrpc-mux'
-const key = '5487d14f0e47ae9cea53fe611e3b8e08d8e0792a5e3b76da1405dad2f726d3a1'
-const publicKey = b4a.from(key, 'hex')
-
 const dht = new DHT()
-const conn = dht.connect(publicKey)
-const framed = new Channel(new Protomux(conn))
 
-conn.once('open', async () => {
-  const api = await framed.request('_swag', {})
-  console.log(api)
-  const el = document.getElementById('editor_holder')
-  const schema = api.routes[0]
-  schema.title = schema.name
-  const editor = new JSONEditor(el, { compact: true, schema }) 
-
-  document.getElementById('submit').addEventListener('click',function() {
-    console.log(editor.getValue());
-  });
-
-  // const schema = await client._swag()
-  console.log(schema)
-
+document.getElementById('connect').addEventListener('click', () => {
+  const peerKey = document.getElementById('peerKey').value
+  destroy()
+  connect(peerKey)
 })
+
+function destroy () {
+    document.getElementById('api').style.display = 'none'
+    document.getElementById('role').innerHTML = ''
+    document.getElementById('version').innerHTML = ''
+    document.getElementById('description').innerHTML = ''
+}
+
+function connect (peerKey) {
+  const publicKey = b4a.from(peerKey, 'hex')
+  const conn = dht.connect(publicKey)
+  const framed = new Channel(new Protomux(conn))
+  conn.once('open', async () => {
+    const api = await framed.request('_swag', {})
+
+    console.log(api)
+
+    document.getElementById('api').style.display = 'block'
+    document.getElementById('role').innerHTML = api.role
+    document.getElementById('version').innerHTML = api.version
+    document.getElementById('description').innerHTML = api.description
+
+    const routesHolder = document.getElementById('routes')
+    api.routes.forEach(route => {
+      console.log(route)
+      const routeDiv = document.createElement('div')
+
+
+
+      routesHolder.appendChild(routeDiv)
+      const editor = new JSONEditor(routeDiv, { compact: true, schema: route.paramSchema }) 
+    })
+
+    // const el = document.getElementById('editor_holder')
+    // const schema = api.routes[0]
+    // schema.title = schema.name
+    // const editor = new JSONEditor(el, { compact: true, schema }) 
+
+    // document.getElementById('submit').addEventListener('click',function() {
+    //   console.log(editor.getValue());
+    // });
+
+    // const schema = await client._swag()
+    // console.log(schema)
+  })
+}
+
 
 
 
